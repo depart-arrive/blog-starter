ad7e61937eaf06cff66224ef33a215af1516b020
depart-arrive <83715816+depart-arrive@users.noreply.github.com>
2021-05-15 14:23:29 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,5 @@
-import { ThemeProvider } from 'next-themes'
-import '../styles.css'
+import '../styles/index.css'
 
-function MyApp({ Component, pageProps }) {
-  return (
-    <ThemeProvider forcedTheme={Component.theme || undefined} attribute="class">
-      <Component {...pageProps} />
-    </ThemeProvider>
-  )
-}
-
-export default MyApp
+ export default function MyApp({ Component, pageProps }) {
+   return <Component {...pageProps} />
+ }
