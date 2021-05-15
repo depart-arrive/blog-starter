3eee6f171ea896a549234f9b3ca1c9ec6ff2a225
depart-arrive <83715816+depart-arrive@users.noreply.github.com>
2021-05-15 18:32:59 +0100

    Update post-preview.js

--- components/post-preview.js
+++ components/post-preview.js
@@ -31,12 +31,10 @@ export default function PostPreview({
         </Link>
       </h3>
       <div className="text-lg mb-4">
-        <DateFormatter dateString={date} />
+        <DateFormatter dateString={date} /> - <Categories categories={categories} />
       </div>
       <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
       <Avatar name={author.name} picture={author.picture} />
-          <Categories categories={categories} />
-
     </div>
   )
 }
