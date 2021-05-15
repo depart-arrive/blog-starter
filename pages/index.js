1d4cda03400f03227a9ea01a14dc939b18a895c0
depart-arrive <83715816+depart-arrive@users.noreply.github.com>
2021-05-15 14:25:38 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,59 +1,51 @@
 import Container from '../components/container'
-import MoreStories from '../components/more-stories'
-import HeroPost from '../components/hero-post'
-import Intro from '../components/intro'
-import Layout from '../components/layout'
-import { getAllPosts } from '../lib/api'
-import Head from 'next/head'
-import { CMS_NAME } from '../lib/constants'
+ import MoreStories from '../components/more-stories'
+ import HeroPost from '../components/hero-post'
+ import Intro from '../components/intro'
+ import Layout from '../components/layout'
+ import { getAllPosts } from '../lib/api'
+ import Head from 'next/head'
+ import { CMS_NAME } from '../lib/constants'
 
-export default function Index({ allPosts }) {
-  const heroPost = allPosts[0]
-  const morePosts = allPosts.slice(1)
-  return (
-    <>
-      <Layout>
-        <Head>
-          <title>Next.js Blog Example with {CMS_NAME}</title>
-            <button
-          className="mt-16 px-4 py-2 text-white dark:text-black bg-black dark:bg-white font-semibold rounded-md"
-          onClick={() => {
-            setTheme(theme === 'light' ? 'dark' : 'light')
-          }}
-        >
-          Change Theme
-        </button>
-        </Head>
-        <Container>
-          <Intro />
-          {heroPost && (
-            <HeroPost
-              title={heroPost.title}
-              coverImage={heroPost.coverImage}
-              date={heroPost.date}
-              author={heroPost.author}
-              slug={heroPost.slug}
-              excerpt={heroPost.excerpt}
-            />
-          )}
-          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
-        </Container>
-      </Layout>
-    </>
-  )
-}
+ export default function Index({ allPosts }) {
+   const heroPost = allPosts[0]
+   const morePosts = allPosts.slice(1)
+   return (
+     <>
+       <Layout>
+         <Head>
+           <title>Next.js Blog Example with {CMS_NAME}</title>
+         </Head>
+         <Container>
+           <Intro />
+           {heroPost && (
+             <HeroPost
+               title={heroPost.title}
+               coverImage={heroPost.coverImage}
+               date={heroPost.date}
+               author={heroPost.author}
+               slug={heroPost.slug}
+               excerpt={heroPost.excerpt}
+             />
+           )}
+           {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+         </Container>
+       </Layout>
+     </>
+   )
+ }
 
-export async function getStaticProps() {
-  const allPosts = getAllPosts([
-    'title',
-    'date',
-    'slug',
-    'author',
-    'coverImage',
-    'excerpt',
-  ])
+ export async function getStaticProps() {
+   const allPosts = getAllPosts([
+     'title',
+     'date',
+     'slug',
+     'author',
+     'coverImage',
+     'excerpt',
+   ])
 
-  return {
-    props: { allPosts },
-  }
-}
+   return {
+     props: { allPosts },
+   }
+ }
