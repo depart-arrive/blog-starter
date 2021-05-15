21cd9251113d56bb2222ebb5f160ca900ba20567
depart-arrive <83715816+depart-arrive@users.noreply.github.com>
2021-05-15 18:07:20 +0100

    Update hero-post.js

--- components/hero-post.js
+++ components/hero-post.js
@@ -39,7 +39,8 @@ export default function HeroPost({
         <div>
           <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
           <Avatar name={author.name} picture={author.picture} />
-          <Categories categories={categories} />
+          <p className="text-lg leading-relaxed mb-4"><Categories categories={categories} /></p>
+          
         </div>
       </div>
     </section>
