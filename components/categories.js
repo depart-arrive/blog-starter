81782383ae8956b1cdb0d521eb968cbfabebe8c5
depart-arrive <83715816+depart-arrive@users.noreply.github.com>
2021-05-15 18:39:36 +0100

    Update categories.js

--- components/categories.js
+++ components/categories.js
@@ -1,15 +1,15 @@
 export default function Categories({ categories }) {
   return (
-    <span className="ml-1">
+    <span className="ml-2">
       
       {categories > 0 ? (
         categories((category, index) => (
-          <span key={index} className="ml-4">
+          <span key={index} className="ml-2">
             {category}
           </span>
         ))
       ) : (
-        <span className="ml-1">{categories} </span>
+        <span className="ml-2">{categories}</span>
       )}
     </span>
   ) 
