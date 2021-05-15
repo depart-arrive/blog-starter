65d8a9ed662d82934f5a8e5038695094a13c91a7
depart-arrive <83715816+depart-arrive@users.noreply.github.com>
2021-05-15 17:33:30 +0100

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -1,67 +1,16 @@
 import Alert from '../components/alert'
-import Footer from '../components/footer'
-import Meta from '../components/meta'
-import Link from '@/components/Link'
-import Tag from '@/components/Tag'
-import siteMetadata from '@/data/siteMetadata'
-import { useState } from 'react'
- 
+ import Footer from '../components/footer'
+ import Meta from '../components/meta'
 
-export default function ListLayout({ posts, title }) {
-  const [searchValue, setSearchValue] = useState('')
-  const filteredBlogPosts = posts.filter((frontMatter) => {
-    const searchContent = frontMatter.title + frontMatter.summary + frontMatter.tags.join(' ')
-    return searchContent.toLowerCase().includes(searchValue.toLowerCase())
-  })
-
-  return (
-
-    <>
-      <Meta />
-      <div className="min-h-screen">
-        <Alert preview={preview} />
-        <main>{children}
-     <ul>
-          {!filteredBlogPosts.length && 'No posts found.'}
-          {filteredBlogPosts.map((frontMatter) => {
-            const { slug, date, title, summary, tags } = frontMatter
-            return (
-              <li key={slug} className="py-4">
-                <article className="space-y-2 xl:grid xl:grid-cols-4 xl:space-y-0 xl:items-baseline">
-                  <dl>
-                    <dt className="sr-only">Published on</dt>
-                    <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
-                      <time dateTime={date}>
-                        {new Date(date).toLocaleDateString(siteMetadata.locale, postDateTemplate)}
-                      </time>
-                    </dd>
-                  </dl>
-                  <div className="space-y-3 xl:col-span-3">
-                    <div>
-                      <h3 className="text-2xl font-bold leading-8 tracking-tight">
-                        <Link href={`/blog/${slug}`} className="text-gray-900 dark:text-gray-100">
-                          {title}
-                        </Link>
-                      </h3>
-                      <div className="flex flex-wrap">
-                        {tags.map((tag) => (
-                          <Tag key={tag} text={tag} />
-                        ))}
-                      </div>
-                    </div>
-                    <div className="prose text-gray-500 max-w-none dark:text-gray-400">
-                      {summary}
-                    </div>
-                  </div>
-                </article>
-              </li>
-            )
-          })}
-        </ul>
-
-</main>
-      </div>
-      <Footer />
-    </>
-  )
-}
+ export default function Layout({ preview, children }) {
+   return (
+     <>
+       <Meta />
+       <div className="min-h-screen">
+         <Alert preview={preview} />
+         <main>{children}</main>
+       </div>
+       <Footer />
+     </>
+   )
+ }
