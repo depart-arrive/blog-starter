f3763c5cf803e3b5d2136e60a2cd51a4227176c0
depart-arrive <83715816+depart-arrive@users.noreply.github.com>
2021-05-15 15:10:20 +0100

    Update [slug].js

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -36,6 +36,7 @@ export default function Post({ post, morePosts, preview }) {
                 coverImage={post.coverImage}
                 date={post.date}
                 author={post.author}
+                    categories={post.categories}
               />
               <PostBody content={post.content} />
             </article>
